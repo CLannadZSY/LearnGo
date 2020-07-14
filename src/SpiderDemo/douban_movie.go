afe280c1faf169243f8e094bae9a94bedfef7a33
CLannadZSY <zsymidi@gmail.com>
2020-07-14 23:34:56 +0800

    使用 htmlquery 模块的 xpath 语法进行解析数据

--- src/SpiderDemo/douban_movie.go
+++ src/SpiderDemo/douban_movie.go
@@ -2,7 +2,11 @@ package main
 
 import (
 	"fmt"
+	"github.com/antchfx/htmlquery"
 	"github.com/gocolly/colly"
+	"golang.org/x/net/html"
+	"log"
+	"strings"
 	"time"
 )
 
@@ -35,12 +39,18 @@ func main() {
 
 	c.OnResponse(func(response *colly.Response) {
 		fmt.Println("OnResponse", response.StatusCode)
+		htmlNodes, err := htmlquery.Parse(strings.NewReader(string(response.Body)))
+		if err != nil {
+			log.Fatal(err)
+		}
+		parseHtml(htmlNodes)
+
 	})
 
-	c.OnHTML("a[href]", func(element *colly.HTMLElement) {
-		movieInfo := element.Attr("href")
-		fmt.Println(movieInfo)
-	})
+	//c.OnHTML("a[href]", func(element *colly.HTMLElement) {
+	//	movieInfo := element.Attr("href")
+	//	fmt.Println(movieInfo)
+	//})
 
 	c.OnError(func(response *colly.Response, err error) {
 		fmt.Println(err)
@@ -50,4 +60,18 @@ func main() {
 	c.Visit(startURL)
 
 	c.Wait()
+}
+
+// 使用 htmlquery 模块的 xpath 语法进行解析数据
+func parseHtml(html *html.Node) {
+
+	nodes := htmlquery.Find(html, "//div[@class='info']")
+	for _, node := range nodes {
+		url := htmlquery.FindOne(node, ".//a/@href")
+		title := htmlquery.FindOne(node, ".//span[@class='title']/text()")
+		urlText := htmlquery.InnerText(url)
+		titleText := htmlquery.InnerText(title)
+		fmt.Println(urlText)
+		fmt.Println(titleText)
+	}
 }
