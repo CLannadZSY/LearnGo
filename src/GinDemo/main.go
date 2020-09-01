9c8ec523f52f1173d44bd89edee5c106c1f31bd9
CLannadZSY <zsymidi@gmail.com>
2020-09-01 20:34:00 +0800

    提供数据的三种方式

--- src/GinDemo/main.go
+++ src/GinDemo/main.go
@@ -2,8 +2,6 @@ package main
 
 import (
 	"github.com/gin-gonic/gin"
-	"github.com/gin-gonic/gin/testdata/protoexample"
-	"net/http"
 )
 
 func main() {
@@ -315,45 +313,79 @@ func main() {
 
 	// 21. XML, JSON, YAML and ProtoBuf rendering
 	// gin.H is a shortcut for map[string]interface{}
-	router.GET("/someJSON", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
-	})
+	//router.GET("/someJSON", func(c *gin.Context) {
+	//	c.JSON(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
+	//})
+	//
+	//router.GET("/moreJSON", func(c *gin.Context) {
+	//	// You also can use a struct
+	//	var msg struct {
+	//		Name    string `json:"user"`
+	//		Message string
+	//		Number  int
+	//	}
+	//	msg.Name = "Lena"
+	//	msg.Message = "hey"
+	//	msg.Number = 123
+	//	// Note that msg.Name becomes "user" in the JSON
+	//	// Will output  :   {"user": "Lena", "Message": "hey", "Number": 123}
+	//	c.JSON(http.StatusOK, msg)
+	//})
+	//
+	//router.GET("/someXML", func(c *gin.Context) {
+	//	c.XML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
+	//})
+	//
+	//router.GET("/someYAML", func(c *gin.Context) {
+	//	c.YAML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
+	//})
+	//
+	//router.GET("/someProtoBuf", func(c *gin.Context) {
+	//	reps := []int64{int64(1), int64(2)}
+	//	label := "test"
+	//	// The specific definition of protobuf is written in the testdata/protoexample file.
+	//	data := &protoexample.Test{
+	//		Label: &label,
+	//		Reps:  reps,
+	//	}
+	//	// Note that data becomes binary data in the response
+	//	// Will output protoexample.Test protobuf serialized data
+	//	c.ProtoBuf(http.StatusOK, data)
+	//})
 
-	router.GET("/moreJSON", func(c *gin.Context) {
-		// You also can use a struct
-		var msg struct {
-			Name    string `json:"user"`
-			Message string
-			Number  int
-		}
-		msg.Name = "Lena"
-		msg.Message = "hey"
-		msg.Number = 123
-		// Note that msg.Name becomes "user" in the JSON
-		// Will output  :   {"user": "Lena", "Message": "hey", "Number": 123}
-		c.JSON(http.StatusOK, msg)
-	})
+	// 22. Serving static files 提供静态文件
+	//router.Static("/assets", "./assets")
+	//router.StaticFS("/more_static", http.Dir("my_file_system"))
+	//router.StaticFile("/favicon.ico", "./resources/favicon.ico")
 
-	router.GET("/someXML", func(c *gin.Context) {
-		c.XML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
-	})
+	//23. Serving data from file
+	//router.GET("/local/file", func(c *gin.Context) {
+	//	c.File("local/file.go")
+	//})
+	//
+	//var fs http.FileSystem = // ...
+	//router.GET("/fs/file", func(c *gin.Context) {
+	//	c.FileFromFS("fs/file.go", fs)
+	//})
 
-	router.GET("/someYAML", func(c *gin.Context) {
-		c.YAML(http.StatusOK, gin.H{"message": "hey", "status": http.StatusOK})
-	})
-
-	router.GET("/someProtoBuf", func(c *gin.Context) {
-		reps := []int64{int64(1), int64(2)}
-		label := "test"
-		// The specific definition of protobuf is written in the testdata/protoexample file.
-		data := &protoexample.Test{
-			Label: &label,
-			Reps:  reps,
-		}
-		// Note that data becomes binary data in the response
-		// Will output protoexample.Test protobuf serialized data
-		c.ProtoBuf(http.StatusOK, data)
-	})
+	// 24. Serving data from reader
+	//router.GET("/someDataFromReader", func(c *gin.Context) {
+	//	response, err := http.Get("https://raw.githubusercontent.com/gin-gonic/logo/master/color.png")
+	//	if err != nil || response.StatusCode != http.StatusOK {
+	//		c.Status(http.StatusServiceUnavailable)
+	//		return
+	//	}
+	//
+	//	reader := response.Body
+	//	contentLength := response.ContentLength
+	//	contentType := response.Header.Get("Content-Type")
+	//
+	//	extraHeaders := map[string]string{
+	//		"Content-Disposition": `attachment; filename="gopher.png"`,
+	//	}
+	//
+	//	c.DataFromReader(http.StatusOK, contentLength, contentType, reader, extraHeaders)
+	//})
 
 	router.Run(":8080")
 
