f8b048e61761334942c9a369a17ff418d499fe54
CLannadZSY <zsymidi@gmail.com>
2020-09-06 19:39:50 +0800

    BasicAuth() middleware

--- src/GinDemo/main.go
+++ src/GinDemo/main.go
@@ -1,9 +1,8 @@
 package main
 
 import (
-	"GinDemo/middleware"
 	"github.com/gin-gonic/gin"
-	"log"
+	"net/http"
 )
 
 func main() {
@@ -430,13 +429,41 @@ func main() {
 	//})
 
 	// 28. Custom Middleware
-	router.Use(middleware.Logger())
+	//router.Use(middleware.Logger())
+	//
+	//router.GET("/test", func(c *gin.Context) {
+	//	example := c.MustGet("example").(string)
+	//
+	//	// it would print: "12345"
+	//	log.Println(example)
+	//})
 
-	router.GET("/test", func(c *gin.Context) {
-		example := c.MustGet("example").(string)
+	// 29. Using BasicAuth() middleware
+	var secrets = gin.H{
+		"foo":    gin.H{"email": "foo@bar.com", "phone": "123433"},
+		"austin": gin.H{"email": "austin@example.com", "phone": "666"},
+		"lena":   gin.H{"email": "lena@guapa.com", "phone": "523443"},
+	}
 
-		// it would print: "12345"
-		log.Println(example)
+	// Group using gin.BasicAuth() middleware
+	// gin.Accounts is a shortcut for map[string]string
+	authorized := router.Group("/admin", gin.BasicAuth(gin.Accounts{
+		"foo":    "bar",
+		"austin": "1234",
+		"lena":   "hello2",
+		"manu":   "4321",
+	}))
+
+	// /admin/secrets endpoint
+	// hit "localhost:8080/admin/secrets
+	authorized.GET("/secrets", func(c *gin.Context) {
+		// get user, it was set by the BasicAuth middleware
+		user := c.MustGet(gin.AuthUserKey).(string)
+		if secret, ok := secrets[user]; ok {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": secret})
+		} else {
+			c.JSON(http.StatusOK, gin.H{"user": user, "secret": "NO SECRET :("})
+		}
 	})
 
 	router.Run(":8088")
