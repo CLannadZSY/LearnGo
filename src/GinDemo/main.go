2b930fa1789f64294bb6d63999cb0c0ad7eeadef
CLannadZSY <zsymidi@gmail.com>
2020-09-09 20:37:11 +0800

    Manually Graceful shutdown or restart

--- src/GinDemo/main.go
+++ src/GinDemo/main.go
@@ -1,19 +1,21 @@
 package main
 
 import (
-	_ "github.com/gin-gonic/autotls"
 	"github.com/gin-gonic/gin"
 	_ "golang.org/x/crypto/acme/autocert"
-	"golang.org/x/sync/errgroup"
+	"golang.org/x/net/context"
 	"log"
 	_ "log"
 	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
 	"time"
 )
 
 func main() {
 	// 使用 New() 替代 Default()
-	//router := gin.New()
+	router := gin.New()
 	//router := gin.Default()
 
 	// 1. 最简单示例
@@ -513,77 +515,115 @@ func main() {
 	//log.Fatal(autotls.RunWithManager(router, &m))
 
 	// 33. Run multiple service using Gin
-	server01 := &http.Server{
-		Addr:         ":8080",
-		Handler:      router01(),
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
+	//server01 := &http.Server{
+	//	Addr:         ":8080",
+	//	Handler:      router01(),
+	//	ReadTimeout:  5 * time.Second,
+	//	WriteTimeout: 10 * time.Second,
+	//}
+	//
+	//server02 := &http.Server{
+	//	Addr:         ":8081",
+	//	Handler:      router02(),
+	//	ReadTimeout:  5 * time.Second,
+	//	WriteTimeout: 10 * time.Second,
+	//}
+	//
+	//g.Go(func() error {
+	//	err := server01.ListenAndServe()
+	//	if err != nil && err != http.ErrServerClosed {
+	//		log.Fatal(err)
+	//	}
+	//	return err
+	//})
+	//
+	//g.Go(func() error {
+	//	err := server02.ListenAndServe()
+	//	if err != nil && err != http.ErrServerClosed {
+	//		log.Fatal(err)
+	//	}
+	//	return err
+	//})
+	//
+	//if err := g.Wait(); err != nil {
+	//	log.Fatal(err)
+	//}
 
-	server02 := &http.Server{
-		Addr:         ":8081",
-		Handler:      router02(),
-		ReadTimeout:  5 * time.Second,
-		WriteTimeout: 10 * time.Second,
-	}
-
-	g.Go(func() error {
-		err := server01.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			log.Fatal(err)
-		}
-		return err
+	// 34. Manually Graceful shutdown or restart
+	router.GET("/", func(c *gin.Context) {
+		time.Sleep(5 * time.Second)
+		c.String(http.StatusOK, "Welcome Gin Server")
 	})
 
-	g.Go(func() error {
-		err := server02.ListenAndServe()
-		if err != nil && err != http.ErrServerClosed {
-			log.Fatal(err)
-		}
-		return err
-	})
-
-	if err := g.Wait(); err != nil {
-		log.Fatal(err)
+	srv := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
 	}
 
-	//router.Run(":8088")
+	// Initializing the server in a goroutine so that
+	// it won't block the graceful shutdown handling below
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+			log.Fatalf("listen: %s\n", err)
+		}
+	}()
+
+	// Wait for interrupt signal to gracefully shutdown the server with
+	// a timeout of 5 seconds.
+	quit := make(chan os.Signal)
+	// kill (no param) default send syscall.SIGTERM
+	// kill -2 is syscall.SIGINT
+	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	log.Println("Shutting down server...")
+
+	// The context is used to inform the server it has 5 seconds to finish
+	// the request it is currently handling
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		log.Fatal("Server forced to shutdown:", err)
+	}
+
+	log.Println("Server exiting")
+	router.Run(":8088")
 
 }
 
 // 33. Run multiple service using Gin
-var (
-	g errgroup.Group
-)
-
-func router01() http.Handler {
-	e := gin.New()
-	e.Use(gin.Recovery())
-	e.GET("/", func(c *gin.Context) {
-		c.JSON(
-			http.StatusOK,
-			gin.H{
-				"code":  http.StatusOK,
-				"error": "Welcome server 01",
-			},
-		)
-	})
-
-	return e
-}
-
-func router02() http.Handler {
-	e := gin.New()
-	e.Use(gin.Recovery())
-	e.GET("/", func(c *gin.Context) {
-		c.JSON(
-			http.StatusOK,
-			gin.H{
-				"code":  http.StatusOK,
-				"error": "Welcome server 02",
-			},
-		)
-	})
-
-	return e
-}
+//var (
+//	g errgroup.Group
+//)
+//
+//func router01() http.Handler {
+//	e := gin.New()
+//	e.Use(gin.Recovery())
+//	e.GET("/", func(c *gin.Context) {
+//		c.JSON(
+//			http.StatusOK,
+//			gin.H{
+//				"code":  http.StatusOK,
+//				"error": "Welcome server 01",
+//			},
+//		)
+//	})
+//
+//	return e
+//}
+//
+//func router02() http.Handler {
+//	e := gin.New()
+//	e.Use(gin.Recovery())
+//	e.GET("/", func(c *gin.Context) {
+//		c.JSON(
+//			http.StatusOK,
+//			gin.H{
+//				"code":  http.StatusOK,
+//				"error": "Welcome server 02",
+//			},
+//		)
+//	})
+//
+//	return e
+//}
