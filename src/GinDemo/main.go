080b72a5628527ffab810efe45a937b9103a9d3b
CLannadZSY <zsymidi@gmail.com>
2020-09-10 21:10:07 +0800

    Set and get a cookie

--- src/GinDemo/main.go
+++ src/GinDemo/main.go
@@ -1,16 +1,10 @@
 package main
 
 import (
+	"fmt"
 	"github.com/gin-gonic/gin"
 	_ "golang.org/x/crypto/acme/autocert"
-	"golang.org/x/net/context"
-	"log"
 	_ "log"
-	"net/http"
-	"os"
-	"os/signal"
-	"syscall"
-	"time"
 )
 
 func main() {
@@ -550,44 +544,57 @@ func main() {
 	//}
 
 	// 34. Manually Graceful shutdown or restart
-	router.GET("/", func(c *gin.Context) {
-		time.Sleep(5 * time.Second)
-		c.String(http.StatusOK, "Welcome Gin Server")
-	})
+	//router.GET("/", func(c *gin.Context) {
+	//	time.Sleep(5 * time.Second)
+	//	c.String(http.StatusOK, "Welcome Gin Server")
+	//})
+	//
+	//srv := &http.Server{
+	//	Addr:    ":8080",
+	//	Handler: router,
+	//}
+	//
+	//// Initializing the server in a goroutine so that
+	//// it won't block the graceful shutdown handling below
+	//go func() {
+	//	if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
+	//		log.Fatalf("listen: %s\n", err)
+	//	}
+	//}()
+	//
+	//// Wait for interrupt signal to gracefully shutdown the server with
+	//// a timeout of 5 seconds.
+	//quit := make(chan os.Signal)
+	//// kill (no param) default send syscall.SIGTERM
+	//// kill -2 is syscall.SIGINT
+	//// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
+	//signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	//<-quit
+	//log.Println("Shutting down server...")
+	//
+	//// The context is used to inform the server it has 5 seconds to finish
+	//// the request it is currently handling
+	//ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	//defer cancel()
+	//if err := srv.Shutdown(ctx); err != nil {
+	//	log.Fatal("Server forced to shutdown:", err)
+	//}
+	//
+	//log.Println("Server exiting")
 
-	srv := &http.Server{
-		Addr:    ":8080",
-		Handler: router,
-	}
-
-	// Initializing the server in a goroutine so that
-	// it won't block the graceful shutdown handling below
-	go func() {
-		if err := srv.ListenAndServe(); err != nil && err != http.ErrServerClosed {
-			log.Fatalf("listen: %s\n", err)
-		}
-	}()
-
-	// Wait for interrupt signal to gracefully shutdown the server with
-	// a timeout of 5 seconds.
-	quit := make(chan os.Signal)
-	// kill (no param) default send syscall.SIGTERM
-	// kill -2 is syscall.SIGINT
-	// kill -9 is syscall.SIGKILL but can't be catch, so don't need add it
-	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
-	<-quit
-	log.Println("Shutting down server...")
-
-	// The context is used to inform the server it has 5 seconds to finish
-	// the request it is currently handling
-	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
-	defer cancel()
-	if err := srv.Shutdown(ctx); err != nil {
-		log.Fatal("Server forced to shutdown:", err)
-	}
-
-	log.Println("Server exiting")
-	router.Run(":8088")
+	// 35. Set and get a cookie
+	//router.GET("/cookie", func(c *gin.Context) {
+	//
+	//	cookie, err := c.Cookie("gin_cookie")
+	//
+	//	if err != nil {
+	//		cookie = "NotSet"
+	//		c.SetCookie("gin_cookie", "test", 3600, "/", "localhost", false, true)
+	//	}
+	//
+	//	fmt.Printf("Cookie value: %s \n", cookie)
+	//})
+	//router.Run(":8088")
 
 }
 
